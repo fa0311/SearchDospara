40de8fe9da7df204fda177c52270445ffdfc0995
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-10-24 03:01:04 +0900

    update sample

--- sample.py
+++ sample.py
@@ -1,15 +1,14 @@
-
 from SearchDospara import SearchDospara
 
-# print(SearchDospara().get("3060ti")[0].get())
-
 output = ""
-for data in SearchDospara().get("3060ti"):
+print('検索したい商品を入力してください 例:3070ti')
+for data in SearchDospara().get(input()):
     item = data.get()
+    output += "名前："
     output += item.itemname
-    output += "\n"
+    output += "\n値段(税込み)："
     output += str(item.uriamt_tax) + "円"
-    output += "\n"
+    output += "\nURL："
     output += "https://www.dospara.co.jp/5shopping/detail_parts.php?ic={itemcode}".format(itemcode=item.itemcode)
     output += "\n"
 print(output)
